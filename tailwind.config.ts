a92a12beb87c8ad793a9821d4780f0b6ad6f5543
zubairgfive198 <zubairgfive198@gmail.com>
2025-12-31 03:04:53 +0500

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,179 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  darkMode: "class",
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      maxWidth: {
+        'screen-xl': '75rem',
+        'screen-2xl': '83.75rem'
+      },
+      boxShadow: {
+        'cause-shadow': '0px 4px 17px 0px #00000008',
+      },
+      transitionDuration: {
+        '150': '150ms',
+      },
+      spacing: {
+        '6.25': '6.25rem',
+        '70%': '70%',
+        '40%': '40%',
+        '30%': '30%',
+        '80%': '80%',
+        8.5: '8.5rem',
+        50: '50rem',
+        51: "54.375rem",
+        25: '35.625rem',
+        29: '28rem',
+        120: '120rem',
+        45: '45rem',
+        94: '22.5rem',
+        85: '21rem',
+        3.75: '3.75rem'
+      },
+      inset: {
+        '5%': '5%',
+        '35%': '35%'
+      },
+      zIndex: {
+        '1': '1',
+        '2': '2',
+        '999': '999'
+      },
+      colors: {
+        primary: "#99E39E",
+        secondary: "#1DC8CD",
+        midnight_text: "#263238",
+        muted: "#d8dbdb",
+        error: "#CF3127",
+        warning: "#F7931A",
+        light_grey: "#505050",
+        grey: "#F5F7FA",
+        dark_grey: "#1E2229",
+        border: "#E1E1E1",
+        success: "#3cd278",
+        section: "#737373",
+        darkmode: "#000510",
+        darklight: "#0c372a",
+        dark_border: "#959595",
+        tealGreen : "#477E70",
+        charcoalGray: "#666C78",
+        deepSlate: "#282C36",
+        slateGray: "#2F3543",
+      },
+      fontSize: {
+        86: [
+          "5.375rem",
+          {
+            lineHeight: "1.2",
+          }
+        ],
+        76: [
+          "4.75rem",
+          {
+            lineHeight: "1.2",
+          }
+        ],
+        70: [
+          "4.375rem",
+          {
+            lineHeight: "1.2",
+          }
+        ],
+        54: [
+          "3.375rem",
+          {
+            lineHeight: "1.2",
+          }
+        ],
+        44: [
+          "2.75rem",
+          {
+            lineHeight: "1.3",
+          }
+        ],
+        40: [
+          "2.5rem",
+          {
+            lineHeight: "3rem",
+          },
+        ],
+        36: [
+          "2.25rem",
+          {
+            lineHeight: "2.625rem",
+          },
+        ],
+        30: [
+          "1.875rem",
+          {
+            lineHeight: "2.25rem",
+          },
+        ],
+        28: [
+          "1.75rem",
+          {
+            lineHeight: "2.25rem",
+          },
+        ],
+        24: [
+          "1.5rem",
+          {
+            lineHeight: "2rem",
+          },
+        ],
+        22: [
+          "1.375rem",
+          {
+            lineHeight: "2rem",
+          },
+        ],
+        21: [
+          "1.3125rem",
+          {
+            lineHeight: "1.875rem",
+          },
+        ],
+        18: [
+          "1.125rem",
+          {
+            lineHeight: "1.5rem",
+          },
+        ],
+        17: [
+          "1.0625rem",
+          {
+            lineHeight: "1.4875rem",
+          },
+        ],
+        16: [
+          "1rem",
+          {
+            lineHeight: "1.6875rem",
+          },
+        ],
+        14: [
+          "0.875rem",
+          {
+            lineHeight: "1.225rem",
+          },
+        ],
+      },
+      backgroundImage: {
+        "start": "url('/images/work/bg-start.png')",
+        "perk": "url('/images/perks/perk-bg.png')",
+      },
+      blur: {
+        220: '220px',
+        400: '400px',
+      }
+    },
+  },
+  plugins: [],
+};
+export default config;
